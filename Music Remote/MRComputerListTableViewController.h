1c34cbfaae9db420680033c46ba313a05d4f73e7
Michael Little <bio595@gmail.com>
2013-12-31 03:25:36 +1300

    got a master detail flow

---
+++ Music Remote/MRComputerListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MRComputerListTableViewController.h
+//  Music Remote
+//
+//  Created by Mike on 31/12/13.
+//  Copyright (c) 2013 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRComputerListTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+
+@end
